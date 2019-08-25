4c34becb6161812b5ccb5654d98fc1bcf521dcb5
AudreyMeng <mengkejing0519@gmail.com>
2019-08-25 01:32:50 -0700

    a simple calculator with clear button

--- calculator/scripts/calculator.js
+++ calculator/scripts/calculator.js
@@ -1,27 +1,92 @@
 var Calculator = /** @class */ (function () {
-    function Calculator(xId, yId, outputId) {
-        this.x = document.getElementById(xId);
-        this.y = document.getElementById(yId);
+    function Calculator(outputId) {
+        this.firstNum = 0;
+        this.secondNum = 0;
+        this.operator = "+";
+        this.first = true;
         this.output = document.getElementById(outputId);
+        this.output.innerHTML = " ";
         this.wireEvents();
     }
+    Calculator.prototype.getValue = function (num) {
+        if (this.first) {
+            return this.firstNum = this.firstNum * 10 + num;
+        }
+        else {
+            return this.secondNum = this.secondNum * 10 + num;
+        }
+    };
     Calculator.prototype.wireEvents = function () {
         var _this = this;
-        document.getElementById('Add').addEventListener('click', function (event) {
-            _this.output.innerHTML = _this.add(parseInt(_this.x.value), parseInt(_this.y.value)).toString();
+        var nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+        var _loop_1 = function (n) {
+            var btn = 'button' + n;
+            document.getElementById(btn).addEventListener('click', function (event) {
+                _this.output.innerHTML = _this.getValue(n).toString();
+            });
+        };
+        for (var _i = 0, nums_1 = nums; _i < nums_1.length; _i++) {
+            var n = nums_1[_i];
+            _loop_1(n);
+        }
+        document.getElementById('add').addEventListener('click', function (event) {
+            _this.operator = "+";
+            _this.first = false;
         });
-        document.getElementById('Subtract').addEventListener('click', function (event) {
-            _this.output.innerHTML = _this.subtract(parseInt(_this.x.value), parseInt(_this.y.value)).toString();
+        document.getElementById('subtract').addEventListener('click', function (event) {
+            _this.operator = "-";
+            _this.first = false;
+        });
+        document.getElementById('multiply').addEventListener('click', function (event) {
+            _this.operator = "x";
+            _this.first = false;
+        });
+        document.getElementById('divide').addEventListener('click', function (event) {
+            _this.operator = "/";
+            _this.first = false;
+        });
+        document.getElementById('equal').addEventListener('click', function (event) {
+            _this.output.innerHTML = _this.equal().toString();
+        });
+        document.getElementById('clear').addEventListener('click', function (event) {
+            _this.output.innerHTML = _this.clear().toString();
         });
     };
-    Calculator.prototype.add = function (x, y) {
-        return x + y;
+    Calculator.prototype.add = function () {
+        return this.firstNum + this.secondNum;
     };
-    Calculator.prototype.subtract = function (x, y) {
-        return x - y;
+    Calculator.prototype.subtract = function () {
+        return this.firstNum - this.secondNum;
+    };
+    Calculator.prototype.multiply = function () {
+        return this.firstNum * this.secondNum;
+    };
+    Calculator.prototype.divide = function () {
+        if (this.secondNum === 0) {
+            return "NaN";
+        }
+        return this.firstNum / this.secondNum;
+    };
+    Calculator.prototype.equal = function () {
+        switch (this.operator) {
+            case "-":
+                return this.subtract();
+            case "x":
+                return this.multiply();
+            case "/":
+                return this.divide();
+            default:
+                return this.add();
+        }
+    };
+    Calculator.prototype.clear = function () {
+        this.firstNum = 0;
+        this.secondNum = 0;
+        this.operator = "+";
+        return " ";
     };
     return Calculator;
 }());
 window.onload = function () {
-    var calc = new Calculator('X', 'Y', 'Output');
+    var calc = new Calculator('Output');
 };
--- calculator/scripts/calculator.ts
+++ calculator/scripts/calculator.ts
@@ -1,37 +1,106 @@
 class Calculator {
-    private x : HTMLInputElement;
-    private y : HTMLInputElement;
+    private firstNum = 0;
+    private secondNum = 0;
+    private operator = "+";
     private output : HTMLSpanElement;
+    private first = true;
 
-    constructor(xId: string, yId: string, outputId: string) {
-        this.x = <HTMLInputElement>document.getElementById(xId);
-        this.y = <HTMLInputElement>document.getElementById(yId);
+    constructor(outputId: string) {
         this.output = <HTMLSpanElement>document.getElementById(outputId);
+        this.output.innerHTML = " "
         this.wireEvents();
     }
 
+    getValue(num: number) {
+        if (this.first) {
+            return this.firstNum = this.firstNum * 10 + num;
+        } else {
+            return this.secondNum = this.secondNum * 10 + num;
+        }
+    }
+
     wireEvents() {
-        document.getElementById('Add').addEventListener('click',
+        let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+        for (let n of nums) {
+            let btn = 'button'+n;
+            document.getElementById(btn).addEventListener('click',
+                event => {
+                    this.output.innerHTML = this.getValue(n).toString()
+                });
+        }
+
+        document.getElementById('add').addEventListener('click',
             event => {
-                this.output.innerHTML = this.add(parseInt(this.x.value), parseInt(this.y.value)).toString()
+                this.operator="+"
+                this.first=false
             });
-        document.getElementById('Subtract').addEventListener('click',
+        document.getElementById('subtract').addEventListener('click',
             event => {
-                this.output.innerHTML = this.subtract(parseInt(this.x.value), parseInt(this.y.value)).toString()
+                this.operator="-"
+                this.first=false
+            });
+        document.getElementById('multiply').addEventListener('click',
+            event => {
+                this.operator="x"
+                this.first=false
+            });
+        document.getElementById('divide').addEventListener('click',
+            event => {
+                this.operator="/"
+                this.first=false
+            });
+        document.getElementById('equal').addEventListener('click',
+            event => {
+                this.output.innerHTML = this.equal().toString()
+            });
+        document.getElementById('clear').addEventListener('click',
+            event => {
+                this.output.innerHTML = this.clear().toString()
             });
     }
 
-    add(x: number, y : number) {
-        return x + y;
+    add() {
+        return this.firstNum + this.secondNum;
     }
 
-    subtract(x : number, y: number) {
-        return x - y;
+    subtract() {
+        return this.firstNum - this.secondNum;
+    }
+
+    multiply() {
+        return this.firstNum * this.secondNum;
+    }
+
+    divide() {
+        if (this.secondNum === 0) {
+            return "NaN";
+        }
+        return this.firstNum / this.secondNum;
+    }
+
+    equal() {
+        switch (this.operator) {
+            case "-" :
+                return this.subtract();
+            case "x" :
+                return this.multiply();
+            case "/" :
+                return this.divide();
+            default :
+                return this.add();
+        }
+    }
+
+    clear() {
+        this.firstNum=0
+        this.secondNum=0
+        this.operator="+"
+        return " "
     }
 }
 
 window.onload = function () {
-    var calc = new Calculator('X','Y','Output');
+    var calc = new Calculator('Output');
 };
 
 
